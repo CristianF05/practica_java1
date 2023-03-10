326bf5484e02680713f5f44beb272ef3f3eadd95
CristianF05 <1482670@senati.pe>
2023-03-10 09:44:03 -0500

    Hello Wold en Apache

---
+++ Java_con_NetBeans.java
@@ -0,0 +1,7 @@
+package java_con_netbeans;
+public class Java_con_NetBeans {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        // TODO code application logic here
+    }  
+}
