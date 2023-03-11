6cd72d80c8cd92ada5c299af5d9ad214e728ec01
CristianF05 <1482670@senati.pe>
2023-03-11 10:44:53 -0500

    creacion para la suma de dos datos con entrada de datos

--- Java_con_NetBeans.java
+++ Java_con_NetBeans.java
@@ -2,6 +2,5 @@ package java_con_netbeans;
 public class Java_con_NetBeans {
     public static void main(String[] args) {
         System.out.println("hello World");
-        // TODO code application logic here
     }  
 }
---
+++ java_2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_con_netbeans;
+//Importamos el Scanner para la entrada de datos
+import java.util.Scanner;
+public class java_2 {
+    public static void main(String[] args){
+        
+        
+    }
+    
+}
